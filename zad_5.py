3007f61af1dcaa26bf0a57c73351bcede86d8fd5
DeSpecu <szymkop2001@gmail.com>
2025-06-25 23:21:14 +0200

    zmiana sciezek

--- zad_5.py
+++ zad_5.py
@@ -1,7 +1,18 @@
 import cv2
+import cv2
+import configparser
 
-image = cv2.imread(r"C:\Users\szymk\Downloads\pingwin.png")
-image_gray = cv2.imread(r"C:\Users\szymk\Downloads\pingwin_gray.png")
+config = configparser.ConfigParser()
+try:
+    config.read('config.ini')
+except configparser.Error as e:
+    print(f"Błąd podczas wczytywania pliku konfiguracyjnego: {e}")
+    exit()
+
+path = config['Paths']['image_path']
+
+image = cv2.imread(path)
+image_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Obraz", image)
 cv2.imshow("Obraz w skali szarości", image_gray)
 cv2.waitKey(0)
--- zad_6.py
+++ zad_6.py
@@ -1,6 +1,17 @@
 import cv2
+import cv2
+import configparser
 
-image = cv2.imread(r"C:\Users\szymk\Downloads\pingwin.png")
+config = configparser.ConfigParser()
+try:
+    config.read('config.ini')
+except configparser.Error as e:
+    print(f"Błąd podczas wczytywania pliku konfiguracyjnego: {e}")
+    exit()
+
+path = config['Paths']['image_path']
+
+image = cv2.imread(path)
 
 cv2.namedWindow("Obraz", cv2.WINDOW_NORMAL)
 cv2.imshow("Obraz", image)
